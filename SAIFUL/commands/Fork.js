2258a7f1498090e56afaa89e87f7005499f2ecb2
saiful9733 <saifiul28q@gmail.com>
2025-10-06 18:28:55 +0600

    Update Fork.js

--- SAIFUL/commands/Fork.js
+++ SAIFUL/commands/Fork.js
@@ -15,17 +15,20 @@ module.exports.run = async ({ api, event }) => {
     // 'fork' শব্দ টেক্সটে আছে কি চেক করছে
     if (text.includes("fork")) { 
         const message = `
-📌 *𝐂𝐇𝐀𝐓-𝐁𝐎𝐓-𝐀𝐊𝐀𝐒𝐇 𝐕1*
-
-•𝐑𝐞𝐩𝐨𝐬𝐢𝐭𝐨𝐫𝐲 𝐋𝐢𝐧𝐤:*  
-https://github.com/saiful0144/SAIFUL-BOT-V1.git
-
-কোনো সমস্যা বা প্রশ্ন থাকলে যোগাযোগ করুন:  
-Messenger:https://www.facebook.com/profile.php?id=61577052283173
-
-─────────────────
-Thank you for supporting the Bot!
-─────────────────
+╭───────────────────────────────╮
+│ 🤖 𝐂𝐇𝐀𝐓-𝐁𝐎𝐓-𝐒𝐀𝐈𝐅𝐔𝐋 𝐕1 🚀       │
+├───────────────────────────────┤
+│ 📦 𝗥𝗲𝗽𝗼𝘀𝗶𝘁𝗼𝗿𝘆 𝗟𝗶𝗻𝗸:              │
+│ 🔗 https://github.com/saiful0144/SAIFUL-BOT-V1.git │
+│                                               │
+│ 👑 𝗢𝘄𝗻𝗲𝗿: 𝐒𝐚𝐢𝐟𝐮𝐥 𝐈𝐬𝐥𝐚𝐦 🕊️              │
+│ 💬 যেকোনো তথ্য, পরামর্শ বা সাহায্যের জন্য যোগাযোগ করুন।: │
+│ 📘 Messenger:                                │
+│ 🔹 https://www.facebook.com/profile.php?id=61577052283173 │
+│                                               │
+│ 🛠️ 𝗟𝗮𝘀𝘁 𝗨𝗽𝗱𝗮𝘁𝗲: 𝟬𝟲 𝗢𝗰𝘁 𝟮𝟬𝟮𝟱 ✅           │
+│ 💖 𝗧𝗵𝗮𝗻𝗸𝘀 𝗳𝗼𝗿 𝘀𝘂𝗽𝗽𝗼𝗿𝘁𝗶𝗻𝗴 𝘁𝗵𝗲 𝗕𝗼𝘁! 💫     │
+╰───────────────────────────────╯
 `;
         return api.sendMessage(message, event.threadID, event.messageID);
     }
